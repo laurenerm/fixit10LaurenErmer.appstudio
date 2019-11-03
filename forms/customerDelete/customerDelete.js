94a30c7537b6d12f09948869308631211969c213
Lauren <laurenermer@hotmail.com>
2019-11-03 15:50:16 -0600

    some more

--- forms/customerDelete/customerDelete.js
+++ forms/customerDelete/customerDelete.js
@@ -1,21 +1,44 @@
+/*
+let allCustData = []
+ 
+btnDelete.onclick=function(){
+    let custNameDel = inptCustDel.value
+// make sure the pet name is in the database before you try to delete it
+    let found = false
+    for (i = 0; i <= allCustData.length - 1; i++) {
+        if (custNameDel == allCustData[i][1])
+            found = true
+    }
+    if (found == false) 
+       NSB.MsgBox("That customer name is not in the database.")
+    else if (found == true) {
+      let queryDelete = "DELETE FROM customer WHERE name = " + '"' + custNameDel + '"'
+      req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=lne99312&pass=Lnermer7&database=lne99312&query=" + queryDelete)
+
+      if (req1.status == 200) { //transit worked.
+        if (req1.responseText == 500)    // means the insert succeeded
+            lblDelete.value = "You have successfully deleted the customer named " + custNameDel
+        else
+            NSB.MsgBox("There was a problem deleting " + custNameDel + " from the database.")
+      } else {
+        // transit error
+        NSB.MsgBox("Error: " + req1.status);
+      }  
+  } 
+}
+
 
 customerDelete.onshow=function(){
-       let query = "SELECT name FROM customer"
-    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=clc90595&pass=BIA375&database=clc90595&query=" + query)
+   // get all the data from the database when program loads
+      let query = "SELECT * FROM customer"
+    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=lne99312&pass=Lnermer7&database=lne99312&query=" + query);
 
     if (req1.status == 200) { //transit worked.
-            let results = JSON.parse(req1.responseText)
-            // names now in results array - load names into the dropdown
-            let customer = ''
-            for (i = 0; i <= results.length - 1; i++)
-                txtCustomers.value = customer + results[i][0] + "\n"
+            allCustData = JSON.parse(req1.responseText)
+            console.log("parsed result in onshow:  " + allCustData)
     } else {
         // transit error
         NSB.MsgBox("Error: " + req1.status);
     }  
 }
-
-
-btnDelete.onclick=function(){
-  
-}
+*/
